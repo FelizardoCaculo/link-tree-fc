d0796a393050f20f4a276757a277fd1c4ee64c99
FelizardoCaculo <felizardo1987@gmail.com>
2023-04-10 22:39:19 +0100

    Reset experiments

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* == IMAGE SLIDE SHOW FUNCTION == */
 let i = 0; //start point
 let time = 3000;
-
 //Images List
 let images = [
     'image1.jpg',
@@ -14,18 +13,14 @@ let images = [
     'image8.jpg',
     'image9.jpg'
 ];
-
 //Change Image
 function changeImage() {
     document.slide.src = images[i];
-
     if(i < images.length -1){
         i++;
     }else {
         i = 0;
     }
-
     setTimeout('changeImage()', time);
 }
-
 window.onload = changeImage;
